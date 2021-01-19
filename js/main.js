86a234d0ebf401f9614e94d8483de68404c6ed4b
Taylor Bean <JacobBean18@gmail.com>
2021-01-19 11:01:34 -0500

    Fixed Ln 21 Login to Username

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function doLogin()
 	document.getElementById("loginResult").innerHTML = "";
 
 //	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	var jsonPayload = '{"Username" : "' + login + '", "password" : "' + password + '"}';
 	var url = urlBase + '/Login.' + extension;
 
 	var xhr = new XMLHttpRequest();
