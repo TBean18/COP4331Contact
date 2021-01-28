5aaf5347ab7263cfba98d056ad8ea8e38a50a910
Taylor Bean <JacobBean18@gmail.com>
2021-01-27 23:45:42 -0500

    Created DeleteContact.php

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,46 @@
+<?php
+	$inData = getRequestInfo();
+
+	$contactID = $inData["ContactID"]
+
+	$conn = new mysqli("localhost", "API", "xXPickleAPI4331Xx", "ProjectData");
+	$ret = ""
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "DELETE FROM Contacts WHERE ID = " . $contactID;
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$ret = $conn->info;
+		$conn->close();
+	}
+
+	returnWithInfo($ret);
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo($info){
+		$retValue = '{"info":"' . $info . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
