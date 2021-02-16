b9bfe2c26878678fa9b55184bf18995899bcc495
conradsmi <conradsmith22@protonmail.com>
2021-02-16 13:11:34 -0500

    added logout button

--- js/main.js
+++ js/main.js
@@ -56,6 +56,15 @@ function doLogin()
 
 }
 
+function doLogout()
+{
+    userId = 0;
+    firstName = "";
+    lastName = "";
+    document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href = "index.html";
+}
+
 function saveCookie()
 {
 	var minutes = 20;
